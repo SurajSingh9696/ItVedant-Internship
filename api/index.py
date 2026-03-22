3fc294937bdf826fc3042d256186f8df87ac4d6e
SurajSingh9696 <surajsingh969670@gmail.com>
2026-03-22 14:26:21 +0530

    Set up Django WSGI application and add Vercel auto-migration support; configure SSL headers in settings

--- api/index.py
+++ api/index.py
@@ -2,6 +2,13 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ngo_cms.settings')
 
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
+
+django.setup()
+
+if os.environ.get('VERCEL_AUTO_MIGRATE', '1') == '1':
+    call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
 
 app = get_wsgi_application()
